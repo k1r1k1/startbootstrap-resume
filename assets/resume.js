53a3a4e5d58cbe27819c44a27c293ab89a8a3bf5
kiriki1991 <kirillmerkulov@tut.by>
2018-12-28 14:56:25 +0300

    mobile nav hide fix

--- assets/resume.js
+++ assets/resume.js
@@ -25,11 +25,7 @@ window.onhashchange = function () { // Scrolling
 }
 
 // Closes responsive menu when a scroll trigger link is clicked
-$('.js-scroll-trigger').click(function () {
-	$('.navbar-collapse').collapse('hide');
-});
-
-// Activate scrollspy to add active class to navbar items on scroll
-$('body').scrollspy({
-	target: '#sideNav'
+document.querySelector('.navbar-collapse').addEventListener('click', function() {
+	if (document.querySelector('.navbar-toggler').getAttribute('aria-expanded') == 'true')
+	document.querySelector('.navbar-toggler').click()
 });
